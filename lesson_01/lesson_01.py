e048b0abb50f56d2a69d16b91b04a5104a9ec164
roccoRGB <roccog581@gmail.com>
2025-06-29 00:46:04 +0000

    lists of names in one line

--- lesson_01/lesson_01.py
+++ lesson_01/lesson_01.py
@@ -4,3 +4,6 @@ name01 = "rocco"
 name02 = "drake" 
 name03 = "josh"
 lest = [name00, name01, name02, name03]
+
+#turn your 5 lines of code into 1 line
+lest = ["name00", "name01", "name02", "name03"]
